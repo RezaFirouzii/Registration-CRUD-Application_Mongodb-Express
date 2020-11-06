b010679dda183ef33b15a97b350fce26ad44a737
RezaFirouzii <sr2000f@gmail.com>
2020-11-06 18:26:27 +0330

    POST Request API for new registrations

---
+++ public/scripts/script.js
@@ -0,0 +1,72 @@
+const clientsList = document.getElementById('clients-list');
+const submitButton = document.getElementById('register-btn');
+const userInputs = document.querySelectorAll('.form-control');
+const radioButtons = document.querySelectorAll('.form-check-input');
+
+function resetInputs() {
+    userInputs.forEach(input => {
+        input.value = '';
+    });
+}
+// Template of a list item for a new form
+function getTemplate(doc) {
+    return `<div class="row">
+              <div class="col-md-4">First name: <span class="blue"> ${ doc.firstName }</span></div>
+              <div class="col-md-4">Email: <span class="blue font">${ doc.email }</span></div>
+              <div class="col-md-4 tex-right">
+                <button type="button" class="btn btn-secondary">Edit</button>
+                <button type="button" class="btn btn-danger">Delete</button>
+              </div>
+              <div class="col-md-4">Last name: <span class="blue">${ doc.lastName }</span></div>
+              <div class="col-md-4">Sex: <span class="blue">${ doc.gender }</span></div>
+              <div class="col-md-4"></div>
+              <div class="col-md-4"></div>
+              <div class="col-md-4"></div>
+            </div>`;
+}
+/**
+ * Post Request for registering clients
+ */
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const gender = radioButtons[0].checked ? "MALE" : "FEMALE";
+    fetch('/', {
+        method: "post",
+        body: JSON.stringify({
+            firstName: userInputs[0].value,
+            lastName: userInputs[1].value,
+            email: userInputs[2].value,
+            password: userInputs[3].value,
+            gender: gender
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        return response.json();
+    }).then(jsonResponse => {
+        if (jsonResponse.ok && jsonResponse.n) {
+            const doc = jsonResponse.ops[0];
+            const newForm = document.createElement('li');
+            newForm.classList.add('list-group-item');
+            newForm.innerHTML = getTemplate(doc);
+            clientsList.append(newForm);
+        }
+        resetInputs();
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,18 @@ const db = require('../db');
 const collection = 'Clients';
 
 /* GET Forms */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   db.getDB().collection(collection).find({}).toArray((err, documents) => {
     if (err) console.log(err);
     else res.render('index', { docs: documents });
+  });
+});
+
+/* POST a Form */
+router.post('/', (req, res) => {
+  db.getDB().collection(collection).insertOne(req.body, (err, result) => {
+    if (err) console.log(err);
+    else res.json(result);
   });
 });
 
